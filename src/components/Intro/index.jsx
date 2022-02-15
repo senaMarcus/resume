50f433475829e3abd456cd232cc71d6d861b1a9c
Marcus Sena <maarvins.19@gmail.com>
2022-02-15 19:58:02 -0300

    add: create and set css on my info title

--- src/components/Intro/index.jsx
+++ src/components/Intro/index.jsx
@@ -1,7 +1,26 @@
+import './intro.css'
+
 const Intro = () => {
     return(
         <div>
-            This is intro!
+            <div className="i">
+                <div className="i-left"> 
+                    <div className="i-left-wrapper">
+                        <h2 className="i-intro">Olá, eu me chamo</h2>
+                        <h1 className="i-name">Marcus Sena</h1>    
+                        <div className="i-title">
+                            <div className="i-title-wrapper">
+                                <div className="i-title-item">Engenheiro de Software</div>
+                                <div className="i-title-item">Desenvolvedor Web</div>
+                                <div className="i-title-item">Apaixonado por Astronomia</div>
+                                <div className="i-title-item">Músico</div>
+                                <div className="i-title-item">TECNÓFILO!</div>
+                            </div>
+                        </div>
+                    </div> 
+                </div>
+                <div className="i-right">right</div>
+            </div>
         </div>
     )
 }
