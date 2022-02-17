4bed0e2cf7267ceb726af2f5aac1941abf6c9dd0
Marcus Sena <maarvins.19@gmail.com>
2022-02-17 16:53:31 -0300

    integrate real gmail to recive data from the form

--- src/components/Contact/index.jsx
+++ src/components/Contact/index.jsx
@@ -2,8 +2,23 @@ import './contact.css'
 import Phone from '../../assets/phone.png'
 import Email from '../../assets/email.png'
 import Address from '../../assets/address.png'
+import { useRef } from 'react'
+import emailjs from 'emailjs-com'
 
 const Contact = () => {
+
+    const formRef = useRef()
+
+    const handleSubmit = (e) => {
+        e.preventDefault() //utilizando para quando clicar no btn submit nao dar refresh na pagina
+        emailjs.sendForm('service_oyre5bx', 'template_1cngpy7', formRef.current, 'user_aUpxughc8hZNvvEFbQkLt')
+      .then((result) => {
+          console.log(result.text);
+      }, (error) => {
+          console.log(error.text);
+      });
+    }
+
     return (
         <div className="c">
             <div className="c-bg"></div>    
@@ -41,11 +56,11 @@ const Contact = () => {
                         <p className="c-desc">
                             <b>Qual é a sua história?</b> Entre em contato. Estamos sempre disponíveis caso o projeto certo apareça!
                         </p>
-                        <form>
+                        <form ref={formRef} onSubmit={handleSubmit}>
                             <input type="text" placeholder='Nome' name="user_name" />
                             <input type="text" placeholder='Subject' name="user_subject" />
                             <input type="text" placeholder='Email' name="user_email" />
-                            <textarea rows="5" placeholder='Mensagem' name="mesage"/>
+                            <textarea rows="5" placeholder='Mensagem' name="message"/>
                             <button>Enviar</button>
                         </form>
                     </div>
