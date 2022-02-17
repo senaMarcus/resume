914a1d4e6057c3124eb456038ce57657ea77f814
Marcus Sena <maarvins.19@gmail.com>
2022-02-17 19:02:04 -0300

    add styled component and initial incrementing on the components

--- src/components/About/index.jsx
+++ src/components/About/index.jsx
@@ -1,29 +1,29 @@
-import './about.css'
+import { AboutCard, AboutContent, AboutLeft, AboutRight, CardBg } from './styles.js'
 
 import Front from '../../assets/test.jpg'
 
 const About = () => {
     return(
-        <div className="a">
-            <div className="a-left">
-                <div className="a-card bg"></div>
-                <div className="a-card">
-                    <img src={Front} alt="" className="a-img"/>
-                </div>
-            </div>
-            <div className="a-right">
-                <h1 className="a-title">Sobre mim</h1>
-                <h4 className="a-sub">
+            <AboutContent>
+            <AboutLeft>
+                <CardBg></CardBg>
+                <AboutCard>
+                    <img src={Front} alt="" />
+                </AboutCard>
+            </AboutLeft>
+            <AboutRight>
+                <h1>Sobre mim</h1>
+                <h4>
                     Senta ai, porque lá vem história! 
                 </h4>
-                <p className="a-desc">
+                <p>
                     Me chamo Marcus, tenho 27 anos, nascido numa cidade do interior de Minas Gerais/Brasil chamada Nanuque. A minha paixao pela tecnologia esta no meu sangue. Por algum motivo, durante minha juventude, acabei desviando meu foco da tecnologia para outras áreas, mas de 2020 até os dias de hoje tenho me aprofundado bastante nesse universo. <br/> <br/>
                     Desde 03/2021 venho me imergindo dentro da programação, mais especificamente no mundo FRONTEND, utilizando tecnologias como: HTML, CSS, JAVASCRIPT (frameworks:REACTJS, NODEJS, ...) e muito mais.
                     Implemento aplicativos, sites e landing pages desde o conceito até a implantação, sempre com designs responsivos e técnicas de SEO. <br/>  <br/>
-Sou muito criativo, atento aos detalhes, sempre focado em resolver problemas e ajudar as pessoas. 
+                    Sou muito criativo, atento aos detalhes, sempre focado em resolver problemas e ajudar as pessoas. 
                 </p>
-            </div>
-        </div>
+            </AboutRight>
+            </AboutContent >
     )
 }
 export default About    
---
+++ src/components/About/styles.js
@@ -0,0 +1,57 @@
+import styled from 'styled-components'
+
+export const AboutContent = styled.div`
+        width: 100vw;
+        height: 100vh;
+        display: flex;
+        align-items: center;
+    
+`
+
+export const AboutLeft = styled.div`
+        flex:1;
+        display:flex;
+        align-items: center;
+        justify-content: center;
+        position: relative;
+        height: 100%;
+`
+
+export const CardBg = styled.div`
+        position: absolute; 
+        top: 50px;
+        left: 50px;
+        background-color: #333;
+`
+
+export const AboutRight = styled.div`
+        flex:1;
+
+        h1{
+            font-weight: 600;
+            color:#333;
+        }
+            
+        h4{
+            margin:20px 0px;
+        }
+
+        p{
+            font-size: 13px;
+            font-weight: 300;
+        }
+`
+
+export const AboutCard = styled.div`
+        width: 40%;
+        height: 70vh;
+        border-radius: 30px;
+        position: relative;
+        overflow:hidden;        
+
+        img{
+            width: 100%;
+            height: 100%;
+            object-fit: cover;
+        }
+`
