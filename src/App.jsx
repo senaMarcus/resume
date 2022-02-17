4bcee0df6c3572f9e0a128e0b63ea4db9ddf4cf3
Marcus Sena <maarvins.19@gmail.com>
2022-02-17 16:07:53 -0300

    initial add on contact component

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,7 @@
 import Intro from "./components/Intro";
 import About from "./components/About";
 import ProductList from "./components/ProductList";
+import Contact from "./components/Contact";
 
 function App() {
   return (
@@ -8,6 +9,7 @@ function App() {
       <Intro />   
       <About />
       <ProductList />
+      <Contact />
     </div>
   );
 }
--- src/components/Contact/index.jsx
+++ src/components/Contact/index.jsx
@@ -1,9 +1,50 @@
+import './contact.css'
+import Phone from '../../assets/phone.png'
+import Email from '../../assets/email.png'
+import Address from '../../assets/address.png'
+
 const Contact = () => {
     return (
-        <div>
-            contact
+        <div className="c">
+            <div className="c-bg"></div>    
+                <div className="c-wrapper">
+                    <div className="c-left">
+                        <h1 className="c-title">Quer falar a respeito do seu projeto? Vamos lá!</h1>
+                        <div className="c-info">
+                            <div className="c-info-item">
+                                <img 
+                                    src={Phone} 
+                                    alt="" 
+                                    className="c-icon" 
+                                />
+                                +55 31 99703-7152
+                            </div>
+                            <div className="c-info-item">
+                                <img 
+                                    src={Email} 
+                                    alt="" 
+                                    className="c-icon" 
+                                />
+                                    maarvins.19@gmail.com
+                            </div>
+                            <div className="c-info-item">
+                                <img 
+                                    src={Address} 
+                                    alt="" 
+                                    className="c-icon" 
+                                />
+                                    Entre em contato para nos localizarmos!
+                            </div>
+                        </div>
+                    </div>
+                    <div className="c-right">
+                        <p className="c-desc">
+                            <b>Qual é a sua história?</b> Entre em contato. Estamos sempre disponíveis caso o projeto certo apareça!
+                        </p>
+                    </div>
+                </div>
         </div>
     )
 }
 
-export default Contact
+export default Contact      
