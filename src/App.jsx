588c2e82c1168781cc8f7f59f149126df68b0313
Marcus Sena <maarvins.19@gmail.com>
2022-02-17 19:26:45 -0300

    create a new component Toggle

--- src/App.jsx
+++ src/App.jsx
@@ -2,10 +2,12 @@ import Intro from "./components/Intro";
 import About from "./components/About";
 import ProductList from "./components/ProductList";
 import Contact from "./components/Contact";
+import { Toggle } from "./components/Toggle";
 
 function App() {
   return (
     <div >
+      <Toggle />      
       <Intro />   
       <About />
       <ProductList />
---
+++ src/components/Toggle/index.jsx
@@ -0,0 +1,10 @@
+import './styles.js'
+import { ToggleStyle } from './styles.js'
+
+export const Toggle = () => {
+    return (
+        <ToggleStyle>
+            Toggle
+        </ToggleStyle>
+    )
+}
