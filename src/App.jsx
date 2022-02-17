8b62a61e47feb4909bed593ba3104bf64b6230c8
Marcus Sena <maarvins.19@gmail.com>
2022-02-17 19:48:58 -0300

    adding whatsapp button

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,9 @@ import About from "./components/About";
 import ProductList from "./components/ProductList";
 import Contact from "./components/Contact";
 import { Toggle } from "./components/Toggle";
+import ReactWhatsapp from "react-whatsapp";
+
+import FloatingWhatsApp from "react-floating-whatsapp";
 
 function App() {
   return (
@@ -12,6 +15,8 @@ function App() {
       <About />
       <ProductList />
       <Contact />
+      <ReactWhatsapp number="+5531997037152" message="Olá!!! Como posso te ajudar?" />
+      <FloatingWhatsApp phoneNumber="+5531997037152" />
     </div>
   );
 }
