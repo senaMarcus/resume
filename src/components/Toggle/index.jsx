1b355995fa1eced16900376809cf00106e2277ac
Marcus Sena <maarvins.19@gmail.com>
2022-02-17 22:50:28 -0300

    add configs on the Toggle component

--- src/components/Toggle/index.jsx
+++ src/components/Toggle/index.jsx
@@ -1,10 +1,16 @@
 import './styles.js'
-import { ToggleStyle } from './styles.js'
+import { DarkLightBtn, ToggleStyle } from './styles.js'
+import Sun from '../../assets/sun.png'
+import Moon from '../../assets/moon.png'
 
 export const Toggle = () => {
     return (
         <ToggleStyle>
-            Toggle
+            <img src={Sun} alt="" className="t-icon" />
+            <img src={Moon} alt="" className="t-icon" />
+            <DarkLightBtn>
+                <div className="t-button"></div>
+            </DarkLightBtn>
         </ToggleStyle>
     )
 }
