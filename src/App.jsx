5b9670639b87c4e8c0380c338feaf323a80d2907
Marcus Sena <maarvins.19@gmail.com>
2022-02-18 21:33:13 -0300

    add small config

--- src/App.jsx
+++ src/App.jsx
@@ -5,7 +5,6 @@ import Contact from "./components/Contact";
 import  Toggle  from "./components/Toggle";
 import { ThemeContext } from './context'
 
-import ReactWhatsapp from "react-whatsapp";
 import FloatingWhatsApp from "react-floating-whatsapp";
 import { useContext } from "react";
 
@@ -15,13 +14,12 @@ function App() {
   const darkMode = theme.state.darkMode;
   
   return (
-    <div style={{backgroundColor:darkMode ? "#222" : "white", color: darkMode && "white"}}>
+    <div style={{backgroundColor:darkMode ? "#222" : "white", color: darkMode && "white",}}>
       <Toggle />      
       <Intro />   
       <About />
       <ProductList />
       <Contact />
-      <ReactWhatsapp number="+5531997037152" message="Olá!!! Como posso te ajudar?" />
       <FloatingWhatsApp phoneNumber="+5531997037152" />
     </div>
   );
