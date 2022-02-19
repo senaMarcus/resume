8e49374ba917203c1cf41310c790849220cc3f94
Marcus Sena <maarvins.19@gmail.com>
2022-02-19 17:27:19 -0300

    make responsive the products

--- src/components/Product/index.jsx
+++ src/components/Product/index.jsx
@@ -6,19 +6,22 @@ import { FiGithub } from 'react-icons/fi'
 const Product = ({img,link,title, repository}) => {
     return (
         <div className="p">
+            <div>
+            <p className="card-title">{title} </p>
             <div className="p-browser">
                 <div className="p-circle"></div>
                 <div className="p-circle"></div>
                 <div className="p-circle"></div>
                 <a href={repository} target="_blank" rel="noreferrer" className="p-repo-link">
-                    <p className="card-title">{title}</p>
-                    (Github repository)
+                    {/* <p className="card-title">{title}</p> */}
+                    (Visitar Repositório Github)
                     <FiGithub />
                 </a>
             </div>
             <a href={link} target="_blank" rel="noreferrer">
                 <img src={img} alt="" className="p-img" />
             </a>
+            </div>
         </div>
         
     )
--- src/components/ProductList/index.jsx
+++ src/components/ProductList/index.jsx
@@ -6,7 +6,7 @@ const ProductList = () => {
     return (
         <div className="pl">
             <div className="pl-texts">
-                <h1 className="pl-title">Criar & Inspirar. Isso é <span>MS Developer</span> !</h1>
+                <h1 className="pl-title">Criar & Inspirar. Isso é<br/> <span>MS Developer</span> !</h1>
                 <p className="pl-desc">
                     Tenho esse portifólio como diferencial, contendo projetos estruturados em tecnologia de ponta, landing-pages inovadoras e muito mais ! 
                 </p>
