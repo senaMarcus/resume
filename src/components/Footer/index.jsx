eb1fe86ed882f5a15d4dc7be3e119f8bd238c6d6
Marcus Sena <maarvins.19@gmail.com>
2022-02-20 18:13:24 -0300

    add no reffer

--- src/components/Footer/index.jsx
+++ src/components/Footer/index.jsx
@@ -8,9 +8,9 @@ const Footer = () => {
         <footer class="white-section" id="foter">
             <div class="container-fluid">
                 <ul class="social-icons">
-                    <li ><a href="https://github.com/maarvins" target="_blank" ><i class="fa fa-instagram"><FiGithub/></i></a></li>  
-                    <li><a href="https://www.instagram.com/marcusenadev/" target="_blank"><FiInstagram/><i class="fa fa-linkedin"></i></a></li>  
-                    <li><a href="https://www.linkedin.com/in/marcusena/" target="_blank"><FiLinkedin/><i class="fa fa-github"></i></a></li>  
+                    <li ><a href="https://github.com/maarvins" target="_blank"  rel="noreferrer"><i class="fa fa-instagram"><FiGithub/></i></a></li>  
+                    <li><a href="https://www.instagram.com/marcusenadev/" target="_blank"  rel="noreferrer"><FiInstagram/><i class="fa fa-linkedin"></i></a></li>  
+                    <li><a href="https://www.linkedin.com/in/marcusena/" target="_blank"  rel="noreferrer"><FiLinkedin/><i class="fa fa-github"></i></a></li>  
                 </ul>
                 <p class="copyright">Copyright © 2022 All rights reserved by MarcusenaDev</p>
             </div>
