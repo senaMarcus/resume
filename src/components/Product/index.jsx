73c99bcdf41fcef8dcc5308399ba1eb4dfbc77c8
Marcus Sena <maarvins.19@gmail.com>
2022-02-20 22:41:13 -0300

    change all class name on the product component

--- src/components/Product/index.jsx
+++ src/components/Product/index.jsx
@@ -5,21 +5,21 @@ import { FiGithub } from 'react-icons/fi'
 
 const Product = ({img,link,title, repository}) => {
     return (
-        <div className="p">
+        <div className="product">
             <div>
             <p className="card-title">{title} </p>
-            <div className="p-browser">
-                <div className="p-circle r"></div>
-                <div className="p-circle y"></div>
-                <div className="p-circle g"></div>
-                <a href={repository} target="_blank" rel="noreferrer" className="p-repo-link">
+            <div className="product-browser">
+                <div className="product-circle r"></div>
+                <div className="product-circle y"></div>
+                <div className="product-circle g"></div>
+                <a href={repository} target="_blank" rel="noreferrer" className="product-repo-link">
                     {/* <p className="card-title">{title}</p> */}
                     (Visitar Repositório Github)
                     <FiGithub />
                 </a>
             </div>
             <a href={link} target="_blank" rel="noreferrer">
-                <img src={img} alt="" className="p-img" />
+                <img src={img} alt="" className="product-img" />
             </a>
             </div>
         </div>
