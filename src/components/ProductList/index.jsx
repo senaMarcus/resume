5c63157c797aa78fb4301800fa489d985412ac0a
Marcus Sena <maarvins.19@gmail.com>
2022-02-20 22:36:36 -0300

    change the productlist every class name

--- src/components/ProductList/index.jsx
+++ src/components/ProductList/index.jsx
@@ -4,14 +4,14 @@ import {products} from '../../data'
 
 const ProductList = () => {
     return (
-        <div className="pl">
-            <div className="pl-texts">
-                <h1 className="pl-title">Criar & Inspirar. Isso é<br/> <span>MS Developer</span> !</h1>
-                <p className="pl-desc">
+        <div className="product-list">
+            <div className="product-list-texts">
+                <h1 className="product-list-title">Criar & Inspirar. Isso é<br/> <span>MS Developer</span> !</h1>
+                <p className="product-list-desc">
                     Tenho esse portifólio como diferencial, contendo projetos estruturados em tecnologia de ponta, landing-pages inovadoras e muito mais ! 
                 </p>
             </div>
-            <div className="pl-list">
+            <div className="product-list-list">
                 {products.map((item) => (
                     <Product key={item.id} img={item.img} link={item.link} title={item.title} repository={item.repository}/>
                 ))}                
