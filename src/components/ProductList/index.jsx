e2d06d17ecf19c7a34437c56665742effb26277c
Marcus Sena <maarvins.19@gmail.com>
2022-02-22 18:45:44 -0300

    change the css to scss on the product list component

--- src/components/ProductList/index.jsx
+++ src/components/ProductList/index.jsx
@@ -1,13 +1,13 @@
 import Product from '../Product'
-import './productList.css'
+import './productList.scss'
 import {products} from '../../data'
 
 const ProductList = () => {
     return (
         <div className="product-list">
             <div className="product-list-texts">
-                <h1 className="product-list-title">Criar & Inspirar. Isso é<br/> <span>MS Developer</span> !</h1>
-                <p className="product-list-desc">
+                <h1>Criar & Inspirar. Isso é<br/> <span>MS Developer</span> !</h1>
+                <p>
                     Tenho esse portifólio como diferencial, contendo projetos estruturados em tecnologia de ponta, landing-pages inovadoras e muito mais ! 
                 </p>
             </div>
