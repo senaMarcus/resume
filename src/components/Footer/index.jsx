8c969f50ae96c50245bd6a7138e6edf6afa4e7f4
Marcus Sena <maarvins.19@gmail.com>
2022-02-22 23:59:28 -0300

    make  a new test footer height

--- src/components/Footer/index.jsx
+++ src/components/Footer/index.jsx
@@ -9,8 +9,8 @@ const Footer = () => {
             <div class="container-fluid">
                 <ul class="social-icons">
                     <li ><a href="https://github.com/maarvins" target="_blank"  rel="noreferrer"><i class="fa fa-instagram"><FiGithub/></i></a></li>  
-                    <li><a href="https://www.instagram.com/marcusenadev/" target="_blank"  rel="noreferrer"><FiInstagram/><i class="fa fa-linkedin"></i></a></li>  
-                    <li><a href="https://www.linkedin.com/in/marcusena/" target="_blank"  rel="noreferrer"><FiLinkedin/><i class="fa fa-github"></i></a></li>  
+                    <li><a href="https://www.instagram.com/marcusenadev/" target="_blank"  rel="noreferrer"><i class="fa fa-linkedin"><FiInstagram/></i></a></li>  
+                    <li><a href="https://www.linkedin.com/in/marcusena/" target="_blank"  rel="noreferrer"><i class="fa fa-github"><FiLinkedin/></i></a></li>  
                 </ul>
                 <p class="copyright">Copyright © 2022 All rights reserved by MarcusenaDev</p>
             </div>
